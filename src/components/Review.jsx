a10ea33933c2cdf9e650acf1f98e4639b9a33f5d
jiancpp <117244684+jiancpp@users.noreply.github.com>
2026-02-08 02:48:09 +0800

    Start review component

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -2,13 +2,21 @@ import './Review.css'
 
 function Review({username, reviewTitle, numStars, reviewContent}) {
     return (
-        <div className="post flex">
-            <div>
-                <p><span className="username">{username}</span>  3hrs ago</p>
-                <h1>{reviewTitle}</h1>
-                <p className="description">{reviewContent}</p>
+        <div className="post">
+            <div className='flex'>
+                <div className="profile"></div>
+                <div className="review-details">
+                    <p><span className="username">{username}</span>  3hrs ago</p>
+                    <h1>{reviewTitle}</h1>
+                    <p className="description">{reviewContent}</p>
+                </div>
             </div>
-            <div className="flex"></div>
+            <div className="post-actions flex">
+                <button className="post-btn like">
+                    <i className="bi bi-heart"></i>
+                    <span>55</span>
+                </button>
+            </div>
         </div>
     )
 }
