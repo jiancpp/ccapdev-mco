2d0174b44a116b3a683139f9b2a0944bed1b95f6
jesmaeca <jezzisbored@gmail.com>
2026-02-08 20:49:26 +0800

    Added login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,46 @@
 import "./Login.css"
 
 function Login() {
-    return (
-        <div>
 
+    return (
+        <div id="login">
+            <div className="background">
+                <div className="circle-element circle-l1">
+                </div>
+                <div className="circle-element circle-l2">
+                </div>
+                <div className="circle-element circle-r1">
+                </div>
+                <div className="circle-element circle-r2">
+                </div>
+            </div>
+            <div className="login-background">
+                <div className="logo-container">
+                    <img src="https://eepy-elo.github.io/font-hosting/unsynth-logo.png" alt="unsynth" className="logo"></img>
+                </div>
+                <div className="login">
+                    <div className="login-title">
+                        Login
+                    </div>
+                    <div className="login-forms">
+                        <input type="text" id="user" className="login-details" placeholder="Username/Email"></input>
+                        <input type="password" id="password" className="login-details" placeholder="Password"></input>
+                    </div>
+                    <div className="login-options">
+                        <input type="checkbox" id="remember" className="checkbox"></input>
+                        <div className="remember-me">
+                            Remember Me
+                        </div>
+                        <div className="forgot-password">
+                            <a href="sign-up.html" className="forgot-password-link">Forgot Password?</a>
+                        </div>
+                    </div>
+                    <a href="home.html" className="login-button">LOGIN</a>
+                    <div className="create-account">
+                        <p className="no-account">Don't have an account? <a href="sign-up.html" className="sign-up-link">Sign-up.</a></p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
