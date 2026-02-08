6b9bfb895acaf13e3a4c8ed8270bb81c03c8077b
eepy-elo <shieloheart.lunario@gmail.com>
2026-02-08 16:05:53 +0800

    Added Artists Page

---
+++ src/components/ArtistBlock.jsx
@@ -0,0 +1,23 @@
+import './ArtistBlock.css'
+
+function ArtistBlock({ artist }) {
+    return (
+        <div className="artist-block">
+            <div className='block-content'>
+                <div className="profile"></div>
+                <div className="artist-details">
+                    <div className="top-block">
+                        <h3 className="name">{artist.name}</h3>
+                        <div className="stars">--Rating--</div>
+                    </div>
+                    <div className="bottom-block">
+                        <div className="details">{artist.reviews} reviews</div>
+                        <div className="details">{artist.songs} songs released</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ArtistBlock
