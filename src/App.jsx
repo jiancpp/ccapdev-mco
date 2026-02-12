e69674ba0227899116a0f3f406d214ea16971a98
jiancpp <117244684+jiancpp@users.noreply.github.com>
2026-02-12 16:55:38 +0800

    Add landing page

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import UserProfile from './pages/UserProfile.jsx'
 import ArtistProfile from './pages/ArtistProfile.jsx'
 import ArtistView from './pages/ArtistView.jsx'
 import ScrollToTopWrapper from './components/ScrollToTopWrapper'
+import AboutUnsynth from "./pages/AboutUnsynth";
 
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";  
 
@@ -27,6 +28,7 @@ function App() {
           {/* AUTH PAGES */}
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
+          <Route path='/about' element={<AboutUnsynth />} />
 
           {/* MAIN PAGES (w/ Navbar and Sidebar) */}
           <Route element={<MainLayout activeUserID={activeUser} setActiveUser={setActiveUser}/>}>
