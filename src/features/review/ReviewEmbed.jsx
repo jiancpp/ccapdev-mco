ac4dfed02b1ca81bd494ac097be7bd1f89d5ee28
jiancpp <jiannaclarisse.tm@gmail.com>
2026-03-12 07:17:50 +0800

    edit: variabilize user profile

--- src/features/review/ReviewEmbed.jsx
+++ src/features/review/ReviewEmbed.jsx
@@ -10,38 +10,52 @@ function ReviewEmbed({ review, navigate }) {
     // Artist Review
     console.log("Song: " + review.song_id + "; Album: " + review.album_id);
 
-    if (review.song_id!==null) {
+    if (review.song_id !== null) {
         const song = getSongById(review.song_id);
-        const album = getAlbumById(review.album_id);
-    
-        return (
-            <div className="embed embed-album" onClick={ () => navigate(`/artists/artist-profile/${review.artist_id}`)}>
-                <div className="album-cover">
-                    <img src={album.cover} alt="" />
-                </div>
-                <div className='album-details'>
-                    <span className='album-title'>"{song.title}"</span><br />
-                    <span className='icon'><i className="bi bi-disc-fill"></i></span>
-                    <span>{album.title}</span><br />
-                    <span className='icon'><i className="bi bi-person-fill"></i></span>
-                    <span>{review.artist}</span>
-                </div>
-            </div>
-        )
-    } else if (review.album!==null) {
         const album = getAlbumById(review.album_id);
 
         return (
-            <div className="embed embed-album" onClick={ () => navigate(`/artists/artist-profile/${review.artist_id}`)}>
+            <div className="embed embed-album" onClick={() => navigate(`/artists/artist-profile/${review.artist_id}`)}>
                 <div className="album-cover">
                     <img src={album.cover} alt="" />
                 </div>
                 <div className='album-details'>
-                    <span className='album-title'>{album.title}</span><br />
-                    <span className='icon'><i className="bi bi-person-fill"></i></span>
-                    <span>{review.artist}</span><br />
-                    <span className='icon'><i className="bi bi-music-note-beamed"></i></span>
-                    <span>{album.songs} tracks</span>
+                    <div className="line line-title">
+                        <span className='icon album-title'><i className="bi bi-music-note"></i></span>
+                        <span className='album-title'> {song.title}</span>
+                    </div>
+                    <div className="line">
+                        <span className='icon'><i className="bi bi-person-fill"></i></span>
+                        <span>{review.artist}</span>
+                    </div>
+                    <div className="line">
+                        <span className='icon'><i className="bi bi-disc-fill"></i></span>
+                        <span>{album.title}</span>
+                    </div>
+                </div>
+            </div>
+        )
+    } else if (review.album !== null) {
+        const album = getAlbumById(review.album_id);
+
+        return (
+            <div className="embed embed-album" onClick={() => navigate(`/artists/artist-profile/${review.artist_id}`)}>
+                <div className="album-cover">
+                    <img src={album.cover} alt="" />
+                </div>
+                <div className='album-details'>
+                    <div className="line line-title">
+                        <span className='icon album-title'><i className="bi bi-disc-fill"></i></span>
+                        <span className='album-title'> {album.title}</span>
+                    </div>
+                    <div className="line">
+                        <span className='icon'><i className="bi bi-person-fill"></i></span>
+                        <span>{review.artist}</span>
+                    </div>
+                    <div className="line">
+                        <span className='icon'><i className="bi bi-music-note-beamed"></i></span>
+                        <span>{album.songs} tracks</span>
+                    </div>
                 </div>
             </div>
         )
@@ -52,7 +66,7 @@ function ReviewEmbed({ review, navigate }) {
                 <span>{review.artist}</span>
             </div>
         )
-    }    
+    }
 }
 
 export default ReviewEmbed
