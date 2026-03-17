8b604794300c4e0c5f1c59c04cfee2fb4668eae6
eepy-elo <shieloheart.lunario@gmail.com>
2026-03-18 04:36:27 +0800

    feat: enhance ReviewModal to fetch songs, albums, and artists dynamically

--- src/modals/ReviewModal.jsx
+++ src/modals/ReviewModal.jsx
@@ -1,32 +1,108 @@
 import {
-  RichTextEditorComponent,
-  Toolbar,
-  Image,
-  Link,
-  HtmlEditor,
-  QuickToolbar,
-  Inject
+  RichTextEditorComponent, Toolbar, Image, Link, HtmlEditor, QuickToolbar, Inject
 } from '@syncfusion/ej2-react-richtexteditor';
 import { InteractiveStarRating } from '../components/StarRating';
 import { SearchBar } from '../components/SearchBar';
-import { useState } from 'react';
-
-import { dummySongs } from '../data/dummySongs';
-import { dummyAlbums } from '../data/dummyAlbums';
-import { dummyArtists } from '../data/dummyArtists';
+import { useState, useRef, useEffect } from 'react';
+import { createReview, getAllData, getArtist } from '../api/api';
 
 import './ReviewModal.css';
 
-function ReviewModal({ isOpen, onClose }) {
+function ReviewModal({ isOpen, onClose, activeUserID }) {
     const toolbarSettings = {
         items: ['Bold', 'Italic', 'Underline', 'StrikeThrough', '|', 'Alignments', '|', 'OrderedList', 'UnorderedList', '|', 'CreateLink', 'Image', '|', 'Undo', 'Redo']
     };
 
-    const [selectedItem, setSelectedItem] = useState({
-        title: "SILAKBO",
-        artist_id: "a1",
-        cover: "/assets/Silakbo.jpg"
-    });
+    const [songs, setSongs] = useState([]);
+    const [albums, setAlbums] = useState([]);
+    const [artists, setArtists] = useState([]);
+
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [header, setHeader] = useState("");
+    const [rating, setRating] = useState(0);
+    const rteRef = useRef(null); // Grabs content from  RTE
+
+    useEffect(() => {
+        if (isOpen) {
+            const fetchData = async () => {
+                try {
+                    const [sData, albData, artData] = await Promise.all([
+                        getAllData('songs'),
+                        getAllData('albums'),
+                        getAllData('artists')
+                    ]);
+                    setSongs(sData);
+                    setAlbums(albData);
+                    setArtists(artData);
+                } catch (err) {
+                    console.error("Error loading search data:", err);
+                }
+            };
+            fetchData();
+        } else {
+            setSelectedItem(null); 
+            setHeader("");
+            setRating(0);
+            
+            if (rteRef.current) {
+                rteRef.current.value = ""; 
+            }
+        }
+    }, [isOpen]);
+
+    const handleSelect = (item) => {
+        const isAlbum = !!item.albumName;
+        
+        setSelectedItem({
+            _id: item._id,
+            title: isAlbum ? item.albumName : item.songTitle,
+            artistID: item.artistID, 
+            cover: item.cover,
+            type: isAlbum ? "Album" : "Song"
+        });
+    };
+
+    const handleSubmit = async () => {
+        console.log("Submitting:", { selectedItem, header, rating, rteRef });
+
+        if (!selectedItem || !header || !rating) {
+            return;
+        }
+
+        const reviewData = {
+            user: "69b9202a78b9e30e03f59186",           
+            artist: selectedItem.artistID, 
+            targetType: selectedItem.type, 
+            targetID: selectedItem._id,    
+            review_header: header,
+            review_content: rteRef.current.getHtml(), 
+            rating: rating,
+            likes: 0,
+            dislikes: 0,
+            isEdited: false
+        };
+
+        try {
+            await createReview(reviewData);
+            alert("Review submitted successfully!");
+            
+            setSelectedItem(null); 
+            setHeader("");
+            setRating(0);
+            if (rteRef.current) {
+                rteRef.current.value = ""; 
+            }
+
+            onClose();
+        } catch (error) {
+            alert("Error saving review: " + error.message);
+        }
+    };
+
+    const getArtistName = (id) => {
+        const artist = artists.find(a => a._id === id);
+        return artist ? (artist.name) : "Unknown Artist";
+    };
 
     if (!isOpen) return null;
 
@@ -34,51 +110,73 @@ function ReviewModal({ isOpen, onClose }) {
         <div className="modal-overlay" onClick={onClose}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
                 <button className="close-btn" onClick={onClose}>&times;</button>
-                
                 <h2 className="modal-title">Write a Review</h2>
 
                 <SearchBar 
-                    songs={dummySongs} 
-                    albums={dummyAlbums} 
-                    artists={dummyArtists} 
-                    onSelect={(item) => setSelectedItem(item)} 
+                    songs={songs} 
+                    albums={albums} 
+                    artists={artists} 
+                    onSelect={handleSelect} 
                 />
 
-                <div className="review-row">
-                    <div className="selected-card">
-                        <img src={selectedItem.cover} />
-                        <div className="card-info">
-                            <p className="song-title">{selectedItem.title}</p>
-                            <p className="artist-subtitle">
-                                {dummyArtists.find(a => a._id === selectedItem.artist_id)?.name}
-                            </p>
+                <div className="review-form-area">
+                    <div className="review-row">
+                        <div className="selected-card">
+                            {selectedItem ? (
+                                <img src={selectedItem.cover} alt="Cover" />
+                            ) : (
+                                <div className="empty-box">
+                                </div>
+                            )}
+                            <div className="card-info">
+                                <p className="item-title">
+                                    {selectedItem?.title || ""}
+                                </p>
+                                <span className="artist-subtitle">
+                                    {(() => {
+                                        if (selectedItem) {
+                                            return getArtistName(selectedItem.artistID);
+                                        } else {
+                                            return "";
+                                        }
+                                    })()}
+                                </span>
+                            </div>
+                        </div>
+                        
+                        <div className="rating-section">
+                            <p className="rating-label">Star Rating</p>
+                            <InteractiveStarRating totalStars={5} onRate={(val) => setRating(val)} />
                         </div>
                     </div>
 
-                    <div className="rating-section">
-                        <p className="rating-label">Star Rating</p>
-                        <InteractiveStarRating totalStars={5} />
+                    <div className="input-group">
+                        <input 
+                            type="text" 
+                            className="header-input" 
+                            placeholder="Header"
+                            value={header}
+                            onChange={(e) => setHeader(e.target.value)} 
+                        />
+                        <RichTextEditorComponent 
+                            className="review-rte" 
+                            toolbarSettings={toolbarSettings} 
+                            htmlAttributes={{ "data-gramm": "false", "data-gramm_editor": "false" }}
+                            ref={rteRef}
+                        >
+                            <Inject services={[Toolbar, Image, Link, HtmlEditor, QuickToolbar]} />
+                        </RichTextEditorComponent>
                     </div>
-                </div>
 
-                <div className="input-group">
-                    <input type="text" className="header-input" placeholder="Header" />
-                    <RichTextEditorComponent 
-                        className="review-rte" 
-                        toolbarSettings={toolbarSettings} 
-                        insertLinkSettings={{ target: 'body' }}
-                        insertcoverSettings={{ target: 'body' }}
-                        actionComplete={(args) => {
-                            if (args.requestType === 'OpenDialog') {
-                            }
-                        }}
-                    >
-                        <Inject services={[Toolbar, Image, Link, HtmlEditor, QuickToolbar]} />
-                    </RichTextEditorComponent>
-                </div>
-
-                <div className="footer">
-                    <button className="submit-btn" onClick={onClose}>Submit</button>
+                    <div className="footer">
+                        <button 
+                            className="submit-btn" 
+                            onClick={handleSubmit}
+                            // disabled={!selectedItem || !header || !rating}
+                        >
+                            Submit Review
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
