c964aa4c604ffb99cd0dff4fbb586f4077dcde1b
eepy-elo <shieloheart.lunario@gmail.com>
2026-03-17 21:54:00 +0800

    fix: update server port to 5001

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use('/api/reviews', reviewRoutes);     // allows api to fetch data
 // <TODO:> add middleware to verify admin status //
 
 // Configuration Constants
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const MONGOURL = process.env.MONGO_URL
 
 /**
